97d3fbfd4b9bcef765ee0281dd9a16d5c0c5e1bd
Phl14Fontes <pedrohfontes03@gmail.com>
2021-03-29 15:40:28 -0300

    created list test class

---
+++ IdeaProjects/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -0,0 +1,25 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = funcionario("Joao", 2000.0, "CLT")
+    val pedro = funcionario("Pedro", 1500.0, "PJ")
+    val maria = funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach{ println(it)}
+
+    println("---------------------")
+    println(funcionarios.find { it.nome == "Maria" })
+
+    println("---------------------")
+    funcionarios
+        .sortedBy { it.salario } //por em ordem pelo fator salario
+        .forEach{ println(it)}
+
+    println("---------------------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach{ println(it)}
+
+}
